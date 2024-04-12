26674669ced6a49aee1503b82dfa60394ea30060
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-12 20:41:58 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 let addToy = false;
-
+const urlToys='http://localhost:3000/toys'
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  renderAnimals()
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function renderAnimals(){
+fetch(urlToys)
+.then(res=>res.json())
+.then(toys=>{
+  toys.forEach(toy => {
+    let card = document.createElement('div')
+  card.innerHTML = `<h2>${toy.name}</h2>
+  <img src=${toy.image} class = toy-avatar >
+  <p>${toy.likes}</p>
+  <button class="like-btn" id="${toy.id}">Like ❤️</button>`
+  card.setAttribute('class', 'card')
+  document.getElementById('toy-collection').append(card)
+  console.log(card)
+  });
+  
+})
+}
